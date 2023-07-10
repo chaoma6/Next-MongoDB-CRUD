b39c8323046684da456fc18ebc024e672fbe4463
Chao Ma <machaosydney@gmail.com>
2023-07-10 20:12:01 +1000

    Connect client-side and backend

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,27 +1,64 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
-const page = () => {
+const AddTopic = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !description) {
+      alert('Title and description are required');
+      return;
+    }
+
+    try {
+      const res = await fetch('http://localhost:3000/api/topics', {
+        method: 'POST',
+        headers: { contentType: 'application/json' },
+        body: JSON.stringify({ title, description }),
+      });
+
+      if (res.ok) {
+        router.push('/');
+      } else {
+        throw new Error('Failed to add topic');
+      }
+    } catch (error) {
+      console.log('Error adding topic:', error);
+    }
+  };
+
   return (
-    <>
-      <form className='flex flex-col gap-3'>
-        <input
-          type='text'
-          className='border boder-slate-500 px-8 py-2'
-          placeholder='Topic text'
-        />
+    <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
+      <input
+        type='text'
+        className='border boder-slate-500 px-8 py-2'
+        placeholder='Topic text'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
 
-        <input
-          type='text'
-          className='border boder-slate-500 px-8 py-2'
-          placeholder='Topic Description'
-        />
+      <input
+        type='text'
+        className='border boder-slate-500 px-8 py-2'
+        placeholder='Topic Description'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
 
-        <button className='bg-green-600 text-white font-bold py-3 px-6 w-fit'>
-          Add Topic
-        </button>
-      </form>
-    </>
+      <button
+        type='submit'
+        className='bg-green-600 text-white font-bold py-3 px-6 w-fit'
+      >
+        Add Topic
+      </button>
+    </form>
   );
 };
 
-export default page;
+export default AddTopic;
